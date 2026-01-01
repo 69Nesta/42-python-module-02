5d457698d7039b43555f72d3a7636dad4eb038a4
69Nesta <petitromeo74@gmail.com>
2026-01-01 17:59:57 +0100

    Feat: ft_first_exception

---
+++ ex0/ft_first_exception.py
@@ -0,0 +1,34 @@
+#! python3
+
+def check_temperature(temp_str: str) -> int | None:
+    try:
+        temp = int(temp_str)
+    except ValueError:
+        return print(f'Error: \'{temp_str}\' is not a valid number')
+    else:
+        if (temp > 40):
+            print(f'Error: {temp}°C is too hot for plants (max 40°C)')
+        elif (temp < 0):
+            print(f'Error: {temp}°C is too cold for plants (min 0°C)')
+        else:
+            print(f'Temperature {temp}°C is perfect for plants!')
+            return temp
+
+
+def test_input(value: str):
+    print(f'Testing temperature: \'{value}\'')
+    check_temperature(value)
+    print('')
+
+
+def test_temperature_input() -> None:
+    print('=== Garden Temperature Checker ===\n')
+    test_input("25")
+    test_input("abc")
+    test_input("100")
+    test_input("-50")
+    print('All tests completed - program didn\'t crash!')
+
+
+if __name__ == '__main__':
+    test_temperature_input()
